51c22d9c78052100e5539a098d3e5df1a5a16297
Marcelo Schambeck <mschambeck@gmail.com>
2023-11-06 00:53:41 -0300

    Lombok setter

--- src/main/java/com/schambeck/war/defender/GunDefender.java
+++ src/main/java/com/schambeck/war/defender/GunDefender.java
@@ -3,20 +3,18 @@ package com.schambeck.war.defender;
 import javafx.animation.Timeline;
 import javafx.scene.shape.Cylinder;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
 public class GunDefender extends Cylinder {
     private final DefenderBox defender;
     private final Integer reference;
+    @Setter
     private Timeline timeline;
     public GunDefender(double radius, double height, DefenderBox defender, Integer reference) {
         super(radius, height);
         this.defender = defender;
         this.reference = reference;
         
-    }
-    
-    public void setTimeline(Timeline timeline) {
-        this.timeline = timeline;
     }
 }
