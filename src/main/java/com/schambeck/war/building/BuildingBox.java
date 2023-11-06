984fcc8ec24b92227b8fac9884d771c950941362
Marcelo Schambeck <mschambeck@gmail.com>
2023-11-06 00:51:25 -0300

    Lombok getter

--- src/main/java/com/schambeck/war/building/BuildingBox.java
+++ src/main/java/com/schambeck/war/building/BuildingBox.java
@@ -1,15 +1,13 @@
 package com.schambeck.war.building;
 
 import javafx.scene.shape.Box;
+import lombok.Getter;
 
+@Getter
 public class BuildingBox extends Box {
     private final Integer reference;
     public BuildingBox(double width, double height, double depth, Integer reference) {
         super(width, height, depth);
         this.reference = reference;
-    }
-    
-    public Integer getReference() {
-        return reference;
     }
 }
